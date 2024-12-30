65fee2ab0476b974de68a30ebbaee77a18e660c1
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2024-12-30 16:00:08 -0300

    refactor layout to be shared acress pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-import { Countries, Footer, Header } from "./components";
+import { Countries } from "./components";
 import { CountriesProvider } from "./contexts/CountriesContext";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main className="flex-1">
-        <CountriesProvider>
-          <Countries />
-        </CountriesProvider>
-      </main>
-      <Footer />
-    </>
+    <CountriesProvider>
+      <Countries />
+    </CountriesProvider>
   );
 }
